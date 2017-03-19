62bc85ee65995ca2febe9f13e42a8267f6257c65
Isaac Castillo <icemancast@gmail.com>
2017-03-19 01:17:21 -0500

    lesson files

--- wordpress/wp-content/themes/sitepoint/index.php
+++ wordpress/wp-content/themes/sitepoint/index.php
@@ -1,69 +1,52 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
+<?php get_header(); ?>
+<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="breadcrumb">
+					<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+					<li class="active">Blog</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
 
-get_header(); ?>
+				<?php
+				if ( have_posts() ) :
 
-<div class="wrap">
-	<?php if ( is_home() && ! is_front_page() ) : ?>
-		<header class="page-header">
-			<h1 class="page-title"><?php single_post_title(); ?></h1>
-		</header>
-	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
-	</header>
-	<?php endif; ?>
+					/* Start the Loop */
+					while ( have_posts() ) : the_post(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+						<article>
+							<div class="post-image">
+								<div class="post-heading">
+									<h3><a href="#"><?php the_title(); ?></a></h3>
+								</div>
+								<?php if ( has_post_thumbnail() ) : ?>
+									<?php the_post_thumbnail(); ?>
+								<?php endif; ?>
+							</div>
+							<?php the_excerpt(); ?>
+							<div class="bottom-article">
+								<ul class="meta-post">
+									<li><i class="icon-calendar"></i><a href="#"><?php the_date('F j, Y'); ?></a></li>
+									<li><i class="icon-comments"></i><a href="#"><?php comments_number( 'no comments', 'one comment', '% Comments' ); ?></a></li>
+								</ul>
+								<a href="<?php get_permalink(); ?>" class="pull-right">Continue reading <i class="icon-angle-right"></i></a>
+							</div>
+						</article>
 
-			<?php helloWordl(); ?>
-
-			<?php
-			if ( have_posts() ) :
-
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/post/content', get_post_format() );
-
-				endwhile;
-
-				the_posts_pagination( array(
-					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-				) );
-
-			else :
-
-				get_template_part( 'template-parts/post/content', 'none' );
-
-			endif;
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
-
+				<?php
+					endwhile;
+				endif; ?>
+			</div>
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
+</section>
 <?php get_footer();
